a74c23d75aa5fbebf4aae7cba03e39379ab6de20
Ricardo Fabbri <rfabbri@gmail.com>
2016-12-22 13:25:06 -0200

    VXD: removed test that remained in LEMS

--- contrib/brld/bbasd/bgld/tests/test_driver.cxx
+++ contrib/brld/bbasd/bgld/tests/test_driver.cxx
@@ -3,7 +3,6 @@
 
 DECLARE( test_arc );
 DECLARE( test_poly_line );
-DECLARE( test_minimal_container );
 DECLARE( test_eno_curve );
 DECLARE( test_eno_curve_3d );
 DECLARE( test_linear_curve_3d );
@@ -14,7 +13,6 @@ register_tests()
 {
   REGISTER( test_arc );
   REGISTER( test_poly_line );
-  REGISTER( test_minimal_container );
   //REGISTER( test_eno_curve );
   //REGISTER( test_eno_curve_3d );
   REGISTER( test_linear_curve_3d );
--- contrib/brld/bbasd/bgld/tests/test_include.cxx
+++ contrib/brld/bbasd/bgld/tests/test_include.cxx
@@ -2,7 +2,6 @@
 #include <dbgl/dbgl_eulerspiral_base.h>
 #include <dbgl/dbgl_arc_base.h>
 #include <dbgl/dbgl_arc.h>
-#include <dbgl/dbgl_minimal_container.h>
 #include <dbgl/dbgl_eno_curve.h>
 #include <dbgl/dbgl_eno_curve_3d.h>
 #include <dbgl/dbgl_dist.h>
