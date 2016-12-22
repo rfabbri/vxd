a45740ee329089861b28d67f102efa42ded59500
Ricardo Fabbri <rfabbri@gmail.com>
2016-12-22 13:32:42 -0200

    LEMS to VXD: dbgl/Templates

---
+++ contrib/brld/bbasd/bgld/Templates/dbgl_area+double-.cxx
@@ -0,0 +1,4 @@
+// Instantiation of dbgl_area<double>
+#include <dbgl/dbgl_area.hxx>
+DBGL_AREA_INSTANTIATE(double);
+
---
+++ contrib/brld/bbasd/bgld/Templates/dbgl_area+float-.cxx
@@ -0,0 +1,4 @@
+// Instantiation of dbgl_area<float>
+#include <dbgl/dbgl_area.hxx>
+DBGL_AREA_INSTANTIATE(float);
+
