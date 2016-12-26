546210b2a6614ef9d957c3a7ba601876dd65dfbb
Ricardo Fabbri <rfabbri@gmail.com>
2016-12-26 19:22:02 -0200

    mw to bmcsd

--- contrib/brld/bmvgd/bmcsd/Templates/bmcsd_util+unsigned-.cxx
+++ contrib/brld/bmvgd/bmcsd/Templates/bmcsd_util+unsigned-.cxx
@@ -1,3 +1,3 @@
-// Instantiation of mw_util<double>
-#include <mw/mw_util.hxx>
-MW_UTIL_INSTANTIATE(unsigned);
+// Instantiation of bmcsd_util<double>
+#include <mw/bmcsd_util.hxx>
+BMCSD_UTIL_INSTANTIATE(unsigned);
