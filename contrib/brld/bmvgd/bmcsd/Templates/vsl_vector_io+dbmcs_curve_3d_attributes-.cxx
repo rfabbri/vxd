1b159722ba43c9f20f83400c3bb91a5a5f00d10b
Ricardo Fabbri <rfabbri@gmail.com>
2016-12-26 19:30:26 -0200

    LEMS to VXD: mw/templates

---
+++ contrib/brld/bmvgd/bmcsd/Templates/vsl_vector_io+dbmcs_curve_3d_attributes-.cxx
@@ -0,0 +1,4 @@
+// Instantiation of IO for vcl_list<mw_attributed_point>
+#include <vsl/vsl_vector_io.hxx>
+#include <mw/dbmcs_curve_3d_attributes.h>
+VSL_VECTOR_IO_INSTANTIATE(dbmcs_curve_3d_attributes);
