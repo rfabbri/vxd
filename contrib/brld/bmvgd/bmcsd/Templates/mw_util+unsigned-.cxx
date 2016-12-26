de3aeb3d156289f7f79323e3d167117c63c159b6
Ricardo Fabbri <rfabbri@gmail.com>
2016-12-26 19:19:17 -0200

    LEMS to VXD: mw/templates

---
+++ contrib/brld/bmvgd/bmcsd/Templates/mw_util+unsigned-.cxx
@@ -0,0 +1,3 @@
+// Instantiation of mw_util<double>
+#include <mw/mw_util.hxx>
+MW_UTIL_INSTANTIATE(unsigned);
--- contrib/brld/bmvgd/bmcsd/tests/test_include.cxx
+++ contrib/brld/bmvgd/bmcsd/tests/test_include.cxx
@@ -1,14 +1,7 @@
 #include<bmcsd/bmcsd_util.h>
-#include<bmcsd/bmcsd_curves.h>
 #include<bmcsd/bmcsd_discrete_corresp.h>
 #include<bmcsd/bmcsd_discrete_corresp_3.h>
 #include<bmcsd/bmcsd_discrete_corresp_n.h>
-#include<bmcsd/bmcsd_dist.h>
-#include<bmcsd/bmcsd_epi_interceptor.h>
-#include<dbecl/dbecl_epiband.h>
-#include<dbecl/dbecl_epiband_iterator.h>
-#include<bmcsd/bmcsd_intersection_sets.h>
-#include<bmcsd/bmcsd_subpixel_point_set.h>
 #include<bmcsd/bmcsd_sift_curve.h>
 #include<bmcsd/bmcsd_view_set.h>
 #include<bmcsd/bmcsd_curve_3d_attributes.h>
