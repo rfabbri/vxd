8df31dc1d7c48975b32b2837202bf63ddf882401
Ricardo Fabbri <rfabbri@gmail.com>
2016-12-26 20:59:59 -0200

    LEMS to VXD: mw/templates

---
+++ contrib/brld/bmvgd/bmcsd/Templates/vbl_io_sparse_array+mw_attributed_point.mw_ntuplet-.cxx
@@ -0,0 +1,5 @@
+#include <vsl/vsl_vector_io.h>
+#include <mw/mw_discrete_corresp_n.h>
+#include <vbl/io/vbl_io_sparse_array_base.hxx>
+
+VBL_IO_SPARSE_ARRAY_BASE_INSTANTIATE(mw_match_attribute, mw_ntuplet);
---
+++ contrib/brld/bmvgd/bmcsd/Templates/vsl_pair_io+vcl_pair+mw_ntuplet-mw_attributed_point-.cxx
@@ -0,0 +1,6 @@
+#include <vsl/vsl_vector_io.h>
+#include <mw/mw_discrete_corresp_n.h>
+#include <vsl/vsl_pair_io.hxx>
+
+
+VSL_PAIR_IO_INSTANTIATE(mw_ntuplet, mw_match_attribute);
