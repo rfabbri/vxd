253ab7feb86c37fa651f1e522df40e630dc36266
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-04 19:52:06 -0200

    sdetd_pro builds

--- contrib/brld/bsegd/sdetd/pro/sdetd_sel_storage.cxx
+++ contrib/brld/bsegd/sdetd/pro/sdetd_sel_storage.cxx
@@ -1,25 +1,25 @@
-// This is brcv/seg/dbdet/pro/dbdet_sel_storage.h
+// This is brcv/seg/sdetd/pro/sdetd_sel_storage.h
 
 //:
 // \file
 
-#include "dbdet_sel_storage.h"
+#include "sdetd_sel_storage.h"
 
 //: Return IO version number;
 short
-dbdet_sel_storage::version() const
+sdetd_sel_storage::version() const
 {
   return 1;
 }
 
-dbdet_edgel_chain_list&
-dbdet_sel_storage::all_groups_frags()
+sdetd_edgel_chain_list&
+sdetd_sel_storage::all_groups_frags()
 {
-  vcl_list<dbdet_edgel_chain_list>::iterator l_it = c_groups_.begin();
+  vcl_list<sdetd_edgel_chain_list>::iterator l_it = c_groups_.begin();
   
   for(; l_it != c_groups_.end(); l_it++)
   {
-    dbdet_edgel_chain_list_iter f_it = (*l_it).begin();
+    sdetd_edgel_chain_list_iter f_it = (*l_it).begin();
     for (; f_it != (*l_it).end(); f_it++)
     {
       all_groups_frags_.push_back(*f_it);
@@ -30,8 +30,8 @@ dbdet_sel_storage::all_groups_frags()
 //: Create a copy of the object on the heap.
 // The caller is responsible for deletion
 bpro1_storage* 
-dbdet_sel_storage::clone() const
+sdetd_sel_storage::clone() const
 {
-  return new dbdet_sel_storage(*this);
+  return new sdetd_sel_storage(*this);
 }
 
