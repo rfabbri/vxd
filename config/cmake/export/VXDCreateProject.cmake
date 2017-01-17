1517ab01b554f094c4b76d91db97b2ad0ab8be3d
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-17 13:14:41 -0200

    export cmakelists now VXDCreateProject

--- config/cmake/export/VXDCreateProject.cmake
+++ config/cmake/export/VXDCreateProject.cmake
@@ -1,56 +1,44 @@
-# This VXLCreateProject.cmake file handles the creation of files needed by
-# other client projects that use VXL.  Nothing is built by this
+# This VXDCreateProject.cmake file handles the creation of files needed by
+# other client projects that use VXD.  Nothing is built by this
 # CMakeLists.txt file.  This CMakeLists.txt file must be processed by
-# CMake after all the other CMakeLists.txt files in the VXL tree,
+# CMake after all the other CMakeLists.txt files in the VXD tree,
 # which is why the add_subdirectory(config/cmake/export) command is at the end
 # of the top level CMakeLists.txt file.
 
-# Needed to get non-cached variable settings used in VXLConfig.cmake.in
-include( ${MODULE_PATH}/FindNetlib.cmake )
-include( ${MODULE_PATH}/FindZLIB.cmake )
-include( ${MODULE_PATH}/FindPNG.cmake )
-include( ${MODULE_PATH}/FindJPEG.cmake )
-include( ${MODULE_PATH}/FindTIFF.cmake )
-include( ${MODULE_PATH}/FindGEOTIFF.cmake )
-set( EXPAT_FIND_QUIETLY "YES" )
-include( ${VXL_ROOT_SOURCE_DIR}/contrib/brl/bmods/FindEXPAT.cmake )
-set( EXPAT_FIND_QUIETLY )
-
 # Save library dependencies.
-set(VXL_CMAKE_DOXYGEN_DIR  ${VXL_ROOT_SOURCE_DIR}/config/cmake/doxygen)
+set(VXD_CMAKE_DOXYGEN_DIR  ${VXD_ROOT_SOURCE_DIR}/config/cmake/doxygen)
 
-get_property(VXLTargets_MODULES GLOBAL PROPERTY VXLTargets_MODULES)
+get_property(VXDTargets_MODULES GLOBAL PROPERTY VXDTargets_MODULES)
 
-set(VXL_CONFIG_CMAKE_DIR "share/vxl/cmake")
+set(VXD_CONFIG_CMAKE_DIR "share/vxd/cmake")
 if(${CMAKE_VERSION} VERSION_LESS 2.8.12)
    set(INTERFACE_LINK_OPTION "")
 else()
    set(INTERFACE_LINK_OPTION "EXPORT_LINK_INTERFACE_LIBRARIES")
 endif()
 
-if(VXLTargets_MODULES)
+if(VXDTargets_MODULES)
   export(TARGETS
-    ${VXLTargets_MODULES}
+    ${VXDTargets_MODULES}
     APPEND
-    FILE "${CMAKE_CURRENT_BINARY_DIR}/VXLTargets.cmake"
+    FILE "${CMAKE_CURRENT_BINARY_DIR}/VXDTargets.cmake"
     ${INTERFACE_LINK_OPTION}
   )
-  install(EXPORT ${VXL_INSTALL_EXPORT_NAME} DESTINATION ${VXL_CONFIG_CMAKE_DIR}
+  install(EXPORT ${VXD_INSTALL_EXPORT_NAME} DESTINATION ${VXD_CONFIG_CMAKE_DIR}
           COMPONENT Development)
 endif()
 
-# Create the VXLConfig.cmake file for the build tree.
-configure_file(${VXL_CMAKE_DIR}/VXLConfig.cmake.in
-               ${PROJECT_BINARY_DIR}/VXLConfig.cmake @ONLY)
+# Create the VXDConfig.cmake file for the build tree.
+configure_file(${VXD_CMAKE_DIR}/VXDConfig.cmake.in
+               ${PROJECT_BINARY_DIR}/VXDConfig.cmake @ONLY)
 
-configure_file(${VXL_CMAKE_DIR}/VXLConfig_export.cmake.in
-               ${PROJECT_BINARY_DIR}/config/cmake/export/VXLConfig.cmake
+configure_file(${VXD_CMAKE_DIR}/VXDConfig_export.cmake.in
+               ${PROJECT_BINARY_DIR}/config/cmake/export/VXDConfig.cmake
                @ONLY)
 
 install(FILES
-  ${PROJECT_BINARY_DIR}/config/cmake/export/VXLConfig.cmake
-  ${VXL_CMAKE_DIR}/VXLStandardOptions.cmake
-  ${VXL_CMAKE_DIR}/UseVXL.cmake
-  ${VXL_CMAKE_DIR}/UseVGUI.cmake
-  DESTINATION ${VXL_CONFIG_CMAKE_DIR}
+  ${PROJECT_BINARY_DIR}/config/cmake/export/VXDConfig.cmake
+  ${VXD_CMAKE_DIR}/VXDStandardOptions.cmake
+  ${VXD_CMAKE_DIR}/UseVXD.cmake
+  DESTINATION ${VXD_CONFIG_CMAKE_DIR}
 )
