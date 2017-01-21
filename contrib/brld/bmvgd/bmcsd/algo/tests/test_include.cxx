84c1548079833d5ab9177fa3a2662b1d07e7b5ba
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-21 21:19:37 -0200

    missing .h in tests

--- contrib/brld/bmvgd/bmcsd/algo/tests/test_include.cxx
+++ contrib/brld/bmvgd/bmcsd/algo/tests/test_include.cxx
@@ -1,3 +1,8 @@
-#include<bmcsd/bmcsd_discrete_corresp_n.h>
+#include<bmcsd/algo/bmcsd_algo_util.h>
+#include<bmcsd/algo/bmcsd_curve_stereo.h>
+#include<bmcsd/algo/bmcsd_data.h>
+#include<bmcsd/algo/bmcsd_discrete_corresp_algo.h>
+#include<bmcsd/algo/bmcsd_dt_curve_stereo.h>
+#include<bmcsd/algo/bmcsd_odt_curve_stereo.h>
 
 int main() { return 0; }
