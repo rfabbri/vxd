175e549850482463d206f5663c7368454e8e5a79
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-21 21:52:37 -0200

    dborl -> borld

--- contrib/brld/brecd/borld/Templates/vbl_io_smart_ptr+borld_exp_stat-.cxx
+++ contrib/brld/brecd/borld/Templates/vbl_io_smart_ptr+borld_exp_stat-.cxx
@@ -1,6 +1,6 @@
-#include <dborl/dborl_evaluation.h>
-#include <dborl/dborl_category_info.h>
+#include <borld/borld_evaluation.h>
+#include <borld/borld_category_info.h>
 #include <vbl/io/vbl_io_smart_ptr.hxx>
 
-VBL_IO_SMART_PTR_INSTANTIATE(dborl_exp_stat);
-VBL_IO_SMART_PTR_INSTANTIATE(dborl_category_info_set);
+VBL_IO_SMART_PTR_INSTANTIATE(borld_exp_stat);
+VBL_IO_SMART_PTR_INSTANTIATE(borld_category_info_set);
