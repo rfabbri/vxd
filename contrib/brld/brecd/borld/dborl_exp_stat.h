9b6f08f45d0b9f0930920142bdb7c9c0fb9032ce
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-21 21:44:51 -0200

    dborl - borld

---
+++ contrib/brld/brecd/borld/dborl_exp_stat.h
@@ -0,0 +1,17 @@
+// This is borld_exp_stat.h
+#ifndef borld_exp_stat_h
+#define borld_exp_stat_h
+//:
+//\file
+//\brief  Basic class to hold experiment statistics
+//\author Based on original code by Ozge C Ozcanli
+//\date 11/15/07
+//
+// \verbatim
+//  Modification
+//    Ricardo Fabbri Moved to VXD into a separate class
+// \endverbatim
+//
+
+#endif // borld_exp_stat_h
+
