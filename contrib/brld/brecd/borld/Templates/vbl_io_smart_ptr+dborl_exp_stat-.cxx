39c7c5181f44ccbb991b75ccac43da02a19917d6
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-21 21:52:01 -0200

    LEMS to VXD: template for dborl

---
+++ contrib/brld/brecd/borld/Templates/vbl_io_smart_ptr+dborl_exp_stat-.cxx
@@ -0,0 +1,6 @@
+#include <dborl/dborl_evaluation.h>
+#include <dborl/dborl_category_info.h>
+#include <vbl/io/vbl_io_smart_ptr.hxx>
+
+VBL_IO_SMART_PTR_INSTANTIATE(dborl_exp_stat);
+VBL_IO_SMART_PTR_INSTANTIATE(dborl_category_info_set);
