392daf28a961b6ecb4fdff4db64d1c0f3bc27f18
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-22 16:32:16 -0200

    category info vbl io

--- contrib/brld/bbasd/buld/Templates/vbl_io_smart_ptr+buld_exp_stat-.cxx
+++ contrib/brld/bbasd/buld/Templates/vbl_io_smart_ptr+buld_exp_stat-.cxx
@@ -2,9 +2,3 @@
 #include <vbl/io/vbl_io_smart_ptr.hxx>
 
 VBL_IO_SMART_PTR_INSTANTIATE(buld_exp_stat);
-
-/* XXX todo move to file
-#include <borld/borld_category_info.h>
-#include <vbl/io/vbl_io_smart_ptr.hxx>
-VBL_IO_SMART_PTR_INSTANTIATE(borld_category_info_set);
-*/
---
+++ contrib/brld/brecd/borld/Templates/vbl_smart_ptr+borld_category_info_set-.cxx
@@ -0,0 +1,3 @@
+#include <borld/borld_category_info.h>
+#include <vbl/io/vbl_io_smart_ptr.hxx>
+VBL_IO_SMART_PTR_INSTANTIATE(borld_category_info_set);
