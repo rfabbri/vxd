40cd8a7fd94151f8855c04ef2e2298b086e01baa
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-21 22:31:36 -0200

    LEMS to VXD: dborl basic templates

---
+++ contrib/brld/brecd/borld/Templates/vbl_smart_ptr+dborl_image_description-.cxx
@@ -0,0 +1,5 @@
+#include <dborl/dborl_image_description.h>
+#include <dborl/dborl_image_data_description_base.h>
+#include <vbl/vbl_smart_ptr.hxx>
+
+VBL_SMART_PTR_INSTANTIATE(dborl_image_description);
