e8d795032000c5b7b21a5c13db00f5d9111e7a75
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-21 22:19:13 -0200

    borld builds with basic stats struct

--- contrib/brld/brecd/borld/Templates/vbl_io_smart_ptr+borld_exp_stat-.cxx
+++ contrib/brld/brecd/borld/Templates/vbl_io_smart_ptr+borld_exp_stat-.cxx
@@ -1,6 +1,10 @@
 #include <borld/borld_exp_stat.h>
-#include <borld/borld_category_info.h>
 #include <vbl/io/vbl_io_smart_ptr.hxx>
 
 VBL_IO_SMART_PTR_INSTANTIATE(borld_exp_stat);
+
+/* XXX todo move to file
+#include <borld/borld_category_info.h>
+#include <vbl/io/vbl_io_smart_ptr.hxx>
 VBL_IO_SMART_PTR_INSTANTIATE(borld_category_info_set);
+*/
