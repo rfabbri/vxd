a26a6385cb0da4444a6d15698e74d027b2b94cb0
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-21 22:32:04 -0200

    dborl to borldd

--- contrib/brld/brecd/borld/Templates/vbl_smart_ptr+borld_image_description-.cxx
+++ contrib/brld/brecd/borld/Templates/vbl_smart_ptr+borld_image_description-.cxx
@@ -1,5 +1,5 @@
-#include <dborl/dborl_image_description.h>
-#include <dborl/dborl_image_data_description_base.h>
+#include <borld/borld_image_description.h>
+#include <borld/borld_image_data_description_base.h>
 #include <vbl/vbl_smart_ptr.hxx>
 
-VBL_SMART_PTR_INSTANTIATE(dborl_image_description);
+VBL_SMART_PTR_INSTANTIATE(borld_image_description);
