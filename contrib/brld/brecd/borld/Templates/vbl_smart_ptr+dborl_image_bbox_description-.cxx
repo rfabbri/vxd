38f88d08d06892daed41e3a0dfe1c00ddce190be
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-21 22:41:47 -0200

    LEMS to VXD: many dborl image descr stuff

---
+++ contrib/brld/brecd/borld/Templates/vbl_smart_ptr+dborl_image_bbox_description-.cxx
@@ -0,0 +1,5 @@
+#include <dborl/dborl_image_bbox_description.h>
+#include <vsol/vsol_box_2d.h>
+#include <vbl/vbl_smart_ptr.hxx>
+
+VBL_SMART_PTR_INSTANTIATE(dborl_image_bbox_description);
---
+++ contrib/brld/brecd/borld/Templates/vbl_smart_ptr+dborl_image_mask_description-.cxx
@@ -0,0 +1,4 @@
+#include <dborl/dborl_image_mask_description.h>
+#include <vbl/vbl_smart_ptr.hxx>
+
+VBL_SMART_PTR_INSTANTIATE(dborl_image_mask_description);
