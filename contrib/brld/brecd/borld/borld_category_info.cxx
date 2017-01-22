472e8c3d58e5560349086904e458de77f654a3be
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-21 22:07:25 -0200

    dborl to borld

--- contrib/brld/brecd/borld/borld_category_info.cxx
+++ contrib/brld/brecd/borld/borld_category_info.cxx
@@ -13,13 +13,13 @@
 //
 //
 
-#include "dborl_category_info.h"
+#include "borld_category_info.h"
 
 #include <vul/vul_awk.h>
 #include <vcl_string.h>
 #include <vcl_sstream.h>
 
-dborl_category_info::dborl_category_info(dborl_category_info& other)
+borld_category_info::borld_category_info(borld_category_info& other)
 {
   name_ = other.name_;
   id_ = other.id_;
@@ -27,22 +27,22 @@ dborl_category_info::dborl_category_info(dborl_category_info& other)
   prefix_list_.insert(prefix_list_.begin(), other.prefix_list_.begin(), other.prefix_list_.end());
 }
 
-void dborl_category_info::set_color(vil_rgb<vxl_byte> c)
+void borld_category_info::set_color(vil_rgb<vxl_byte> c)
 {
   color_ = c;
 }
 
-void dborl_category_info::set_prefix_list(vcl_vector<vcl_string>& list)
+void borld_category_info::set_prefix_list(vcl_vector<vcl_string>& list)
 {
   prefix_list_.insert(prefix_list_.begin(), list.begin(), list.end());
 }
 
-void dborl_category_info::add_prefix(vcl_string p)
+void borld_category_info::add_prefix(vcl_string p)
 {
   prefix_list_.push_back(p);
 }
 
-void dborl_category_info::write_xml(vcl_ostream& os)
+void borld_category_info::write_xml(vcl_ostream& os)
 {
   os << "\t\t<category>\n";
   os << "\t\t\t<name>" << name_ << "</name>\n";
@@ -55,13 +55,13 @@ void dborl_category_info::write_xml(vcl_ostream& os)
   os << "\t\t</category>\n";
 }
 
-void dborl_category_info_set::add_category(dborl_category_info_sptr c) 
+void borld_category_info_set::add_category(borld_category_info_sptr c) 
 { 
   cats_.push_back(c); 
 }
 
 //: assumes that we're given a simple file where each line is: <category_name> <category_id> or it is a comment line which start with the char: #
-bool dborl_category_info_set::read_cats_from_a_simple_file(vcl_istream& is)
+bool borld_category_info_set::read_cats_from_a_simple_file(vcl_istream& is)
 {
   vul_awk awk( is );
   if (!awk)
@@ -72,16 +72,16 @@ bool dborl_category_info_set::read_cats_from_a_simple_file(vcl_istream& is)
     if (cat_name[0] == '#') continue;  // skip the comment lines
     vcl_stringstream cat_id(awk[1]);
     int id; cat_id >> id;
-    dborl_category_info_sptr cinfo = new dborl_category_info(cat_name, id);
+    borld_category_info_sptr cinfo = new borld_category_info(cat_name, id);
     cats_.push_back(cinfo);
   }
   return true;
 }
 
 //: return the class given by the name, return NULL if not in the list
-dborl_category_info_sptr dborl_category_info_set::find_category(const vcl_string class_name)
+borld_category_info_sptr borld_category_info_set::find_category(const vcl_string class_name)
 {
-  dborl_category_info_sptr out_c;
+  borld_category_info_sptr out_c;
   for (unsigned i = 0; i < cats_.size(); i++) {
     if (cats_[i]->name_ == class_name) {
       out_c = cats_[i];
@@ -91,34 +91,34 @@ dborl_category_info_sptr dborl_category_info_set::find_category(const vcl_string
   return out_c;
 }
 
-//: Binary io, NOT IMPLEMENTED, signatures defined to use dborl_category_info_set as a brdb_value
-void vsl_b_write(vsl_b_ostream & os, dborl_category_info_set const &ph)
+//: Binary io, NOT IMPLEMENTED, signatures defined to use borld_category_info_set as a brdb_value
+void vsl_b_write(vsl_b_ostream & os, borld_category_info_set const &ph)
 {
   vcl_cerr << "vsl_b_write() -- Binary io, NOT IMPLEMENTED, signatures defined to use brec_part_hierarchy as a brdb_value\n";
   return;
 }
 
-void vsl_b_read(vsl_b_istream & is, dborl_category_info_set &ph)
+void vsl_b_read(vsl_b_istream & is, borld_category_info_set &ph)
 {
   vcl_cerr << "vsl_b_read() -- Binary io, NOT IMPLEMENTED, signatures defined to use brec_part_hierarchy as a brdb_value\n";
   return;
 }
 
-void vsl_b_read(vsl_b_istream& is, dborl_category_info_set* ph)
+void vsl_b_read(vsl_b_istream& is, borld_category_info_set* ph)
 {
   delete ph;
   bool not_null_ptr;
   vsl_b_read(is, not_null_ptr);
   if (not_null_ptr)
   {
-    ph = new dborl_category_info_set();
+    ph = new borld_category_info_set();
     vsl_b_read(is, *ph);
   }
   else
     ph = 0;
 }
 
-void vsl_b_write(vsl_b_ostream& os, const dborl_category_info_set* &ph)
+void vsl_b_write(vsl_b_ostream& os, const borld_category_info_set* &ph)
 {
   if (ph==0)
   {
--- contrib/brld/brecd/borld/borld_category_info_sptr.h
+++ contrib/brld/brecd/borld/borld_category_info_sptr.h
@@ -1,18 +1,18 @@
-// This is brcv/rec/dborl/dborl_category_info_sptr.h
-#ifndef dborl_category_info_sptr_h_
-#define dborl_category_info_sptr_h_
+// This is brcv/rec/borld/borld_category_info_sptr.h
+#ifndef borld_category_info_sptr_h_
+#define borld_category_info_sptr_h_
 //:
 // \file
-// \brief Smart pointer to a dborl_category_info
+// \brief Smart pointer to a borld_category_info
 // \author Ozge Can Ozcanli, (ozge@lems.brown.edu)
 // \date 10/15/07
 //
 
 #include <vbl/vbl_smart_ptr.h>
 
-class dborl_category_info;
-typedef vbl_smart_ptr<dborl_category_info> dborl_category_info_sptr;
-class dborl_category_info_set;
-typedef vbl_smart_ptr<dborl_category_info_set> dborl_category_info_set_sptr;
+class borld_category_info;
+typedef vbl_smart_ptr<borld_category_info> borld_category_info_sptr;
+class borld_category_info_set;
+typedef vbl_smart_ptr<borld_category_info_set> borld_category_info_set_sptr;
 
-#endif // dborl_category_info_sptr_h_
+#endif // borld_category_info_sptr_h_
