a35a9d0f33f87f0a5d3042ad328ceee02b4462f3
Ricardo Fabbri <rfabbri@gmail.com>
2017-01-24 16:56:44 -0200

    vgel: .txx -> hxx

--- old/contrib/gel/vgel/Templates/vgel_multi_view_data+vtol_vertex_2d_sptr-.cxx
+++ old/contrib/gel/vgel/Templates/vgel_multi_view_data+vtol_vertex_2d_sptr-.cxx
@@ -1,5 +1,5 @@
 
-#include <vgel/vgel_multi_view_data.txx>
+#include <vgel/vgel_multi_view_data.hxx>
 #include <vtol/vtol_vertex_2d_sptr.h>
 
 template class vgel_multi_view_data<vtol_vertex_2d_sptr>;
--- old/contrib/gel/vgel/tests/test_template_include.cxx
+++ old/contrib/gel/vgel/tests/test_template_include.cxx
@@ -1,3 +1,3 @@
-#include <vgel/vgel_multi_view_data.txx>
+#include <vgel/vgel_multi_view_data.hxx>
 
 int main() { return 0; }
